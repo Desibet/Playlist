c6b93c6330372c7367f9156aa4332885d8cd48b4
bdesi <bdesi@desibet>
2024-03-11 20:53:43 -0600

    Playlist 07/03/24 para Profesor Alfonso

---
+++ src/main/java/com/mycompany/playlist/Musica.java
@@ -0,0 +1,34 @@
+package com.mycompany.playlist;
+
+public class Musica {
+    String title;
+    String album;
+    String date;
+    int time;
+    
+    //Metodos
+    
+    //Primera forma
+    /*public void Musicas(String _title, String _album, String _date, int _time){
+        title=_title;
+        album=_album;
+        date=_date;
+        time=_time;
+    }*/
+    
+    //Segunda forma
+    public Musica(String title, String album, String date, int time){
+    
+        this.title=title;
+        this.album=album;
+        this.date=date;
+        this.time=time;
+    }
+    
+    public void ShowPlaylist(){
+        System.out.println("Title: " + title);
+        System.out.println("Album: " + album);
+        System.out.println("Date: " + date);
+        System.out.println("Time: " +time);
+    } 
+}
---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.playlist;
+
+
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musica p1=new Musica("Lose you to love me", "Rare", "2019", 4);
+        p1.ShowPlaylist();
+        }
+}
